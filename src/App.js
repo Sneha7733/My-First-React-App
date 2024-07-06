df171add8ba9b0802a58aece456a266ceed91e4e
Sneha7733 <asnehas7721@gmail.com>
2024-07-06 08:18:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './navbar';
 import Sidebar from './Sidebar';
-import Wel_Come from './Wel_Come';
+import WEL_COME from './WEL_COME';
 import Validator from './Validator';
-import Validator_Info from './Validator_Info';
+import ValidatorInfo from './ValidatorInfo';
 import About from './about';
 import './App.css';
 
@@ -16,11 +16,11 @@ function App() {
         <Sidebar />
         <div className="content">
           <Routes>
-            <Route path="/welcome" element={<Wel_Come />} />
+            <Route path="/welcome" element={<WEL_COME />} />
             <Route path="/about" element={<About />} />
-            <Route path="/validator/:id" element={<Validator_Info />} />
+            <Route path="/validator/:id" element={<ValidatorInfo />} />
             <Route path="/validator" element={<Validator />} />
-            <Route path="/" element={<Wel_Come />} />
+            <Route path="/" element={<WEL_COME />} />
           </Routes>
         </div>
       </div>
